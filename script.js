cfafbc8f65bf6b5fb906f26ce2395a8d13a49d15
GatoJz <gatitoj10000@gmail.com>
2024-12-05 13:14:16 -0500

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.querySelector("form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent the form from being actually submitted
-    alert("Form submitted");
-});
